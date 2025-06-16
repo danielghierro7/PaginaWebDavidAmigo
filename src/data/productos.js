a134b4e77a1baeeac2ef61a7d9b2038372de9932
dgabh <danielghierro@gmail.com>
2025-06-16 20:17:14 +0200

    prueba

---
+++ src/data/productos.js
@@ -0,0 +1,20 @@
+export const productos = [
+    {
+        id: 1,
+        nombre: 'Toldo Clásico',
+        precio: 120,
+        imagen: '/img/toldo-clasico.jpg',
+    },
+    {
+        id: 2,
+        nombre: 'Toldo Retráctil',
+        precio: 180,
+        imagen: '/img/toldo-retractil.jpg',
+    },
+    {
+        id: 3,
+        nombre: 'Toldo Vertical',
+        precio: 150,
+        imagen: '/img/toldo-vertical.jpg',
+    },
+];
