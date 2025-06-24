30a775077e28a58e0ded3a01a1938ec7b56f5f80
dgabh <danielghierro@gmail.com>
2025-06-24 07:23:04 +0200

    prueba rafa

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,4 +1,4 @@
-export const productos = [
+export const productosVenta = [
     {
         id: 'CastilloHinchable1',
         nombre: 'Castillo hinchable flotante',
@@ -26,5 +26,37 @@ export const productos = [
         precio: '892€',
         imagen: '/toroMecanico.jpg',
         url: '/productos/toroMecanico'
+    },
+
+
+    {
+        id: 'toroMecanico', // ID único
+        nombre: 'Toro Mecanico',
+        precio: '892€',
+        imagen: '/toroMecanico.jpg',
+        url: '/productos/toroMecanico'
     }
 ];
+
+export const productosAlquiler =[
+    {
+        id: 'Barredora', // ID único
+        nombre: 'Barredora',
+        precio: '350€',
+        imagen: '/Barredeora5.jpg',
+        url: '/productos/Barredora' // <-- Y así sucesivamente
+    },
+
+    {
+        id: 'rafaela', // ID único
+        nombre: 'Rafa Lanzacucarachas',
+        precio: 'Gratis, no lo quiero',
+        imagen: '/rafa.jpg',
+        url: '/productos/rafaela' // <-- Y así sucesivamente
+    }
+
+
+
+]
+
+export const productosTotales = [...productosVenta, ...productosAlquiler];
