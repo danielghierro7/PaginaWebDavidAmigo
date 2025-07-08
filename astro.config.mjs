eb8970bbc388c123b8132cd11905dcf1e1c62330
dgabh <dgabhie3107@g.educaand.es>
2025-07-08 10:11:17 +0200

    Configuro adapter Netlify para Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     integrations: [react()],
+    adapter: netlify(),
     vite: {
         plugins: [tailwindcss()],
     },
